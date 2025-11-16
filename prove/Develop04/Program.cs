2b62c85ad3a31d9f15e35e20fb3a866f159f2ad7
TannerT776 <ttinsley7762001@gmail.com>
2025-11-15 17:49:01 -0700

    Implement main menu for mindfulness app

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,8 +2,61 @@ using System;
 
 class Program
 {
+    // Files: Activity.cs, BreathingActivity.cs, ReflectionActivity.cs, ListingActivity.cs, Program.cs
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        int choice = 0;
+        while (choice != 4)
+        {
+            Console.Clear();
+            Console.WriteLine("Mindfulness App");
+            Console.WriteLine("----------------");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine();
+            Console.Write("Select an option (1-4): ");
+
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out choice))
+            {
+                Console.WriteLine("Please enter a number 1-4. Press Enter to continue.");
+                Console.ReadLine();
+                choice = 0;
+                continue;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    var breathing = new BreathingActivity();
+                    breathing.StartActivity();
+                    break;
+                case 2:
+                    var reflection = new ReflectionActivity();
+                    reflection.StartActivity();
+                    break;
+                case 3:
+                    var listing = new ListingActivity();
+                    listing.StartActivity();
+                    break;
+                case 4:
+                    Console.WriteLine("Exiting program. Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Press Enter to continue.");
+                    Console.ReadLine();
+                    break;
+            }
+
+            if (choice != 4)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to return to the menu...");
+                Console.ReadLine();
+            }
+        }
     }
 }
