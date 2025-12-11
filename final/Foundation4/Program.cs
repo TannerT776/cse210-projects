6d08fe9cbc6e11ddab543d7bd4450142221f811f
TannerT776 <ttinsley7762001@gmail.com>
2025-12-10 18:31:44 -0700

    Refactor Main method and add activity tracking

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        // 3 miles in 30 minutes
+        activities.Add(new Running("12/01/2025", 30, 3.0));
+
+        // 45 minutes at 15 mph
+        activities.Add(new Cycling("12/02/2025", 45, 15.0));
+
+        // 20 laps in 25 minutes
+        activities.Add(new Swimming("12/03/2025", 25, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
